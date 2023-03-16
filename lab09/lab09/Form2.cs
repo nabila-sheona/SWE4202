3bd6d636ba14bab81fee1e4709fb515676f7f485
Nabila Sheona <126275965+sirexcc001@users.noreply.github.com>
2023-03-16 11:12:50 +0600

    login

--- lab09/lab09/Form2.cs
+++ lab09/lab09/Form2.cs
@@ -8,21 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.WebRequestMethods;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
 
 namespace lab09
 {
     public partial class Form2 : Form
     {
-        string cat;
         public Form2()
         {
             InitializeComponent();
         }
         string rep;
-
         private void label3_Click(object sender, EventArgs e)
         {
 
@@ -44,42 +40,37 @@ namespace lab09
                 string name = textBox1.Text;
                 string pass = textBox4.Text;
                 string path = @"C:\Users\nitol\source\repos\lab_tasks\lab09\lab09\TextFile1.txt";
-                using (StreamWriter s1 = File.AppendText(path))
+                string cat = path;
+               //if (File.Exists(path))
+              // {
+                // MessageBox.Show("Username not available!");
+                //}
+                //else
+                if (textBox4.Text != textBox3.Text)
                 {
-                    FileStream nFile = new FileStream($@"C:\Users\nitol\source\repos\lab_tasks\lab09\lab09\TextFile1.txt", FileMode.Create);
-                    if (File.Exists(userName))
-                    {
-                        MessageBox.Show("Username not available!");
-                    }
-                    else if (textBox4.Text != textBox3.Text)
-                    {
-                        MessageBox.Show("Password didn't Match!");
-                    }
-                    else
-                    {
-
-                        MessageBox.Show("created!");
-                        
-
-                        
-                        // string
-                        string s = textBox1.Text + " " + textBox2.Text + " " + textBox4.Text;
-
-                        //  string s = $"p: {pass} n:{name}";
-
-                        //string r=s;
-                        rep = cat + "\n" + s;
-                        s1.WriteLine(rep);
-                        s1.Close();
-
-                        nFile.Close();
-
-                        Form1 f1 = new Form1();
-                        f1.Show();
-                        this.Hide();
-                    }
+                    MessageBox.Show("Password didn't Match!");
                 }
-                
+                else
+                {
+                    
+                    MessageBox.Show("created!");
+                    //FileStream nFile = new FileStream(@"C:\Users\nitol\source\repos\lab_tasks\lab09\lab09\TextFile1.txt", FileMode.Create);
+
+                    //StreamWriter s1 = new StreamWriter(nFile);
+                    // string s = $"p: {userName} n:{pass}";
+                    string s = textBox1.Text+ " " + textBox2.Text + " " + textBox4.Text;
+
+                    File.AppendAllText(path, name);
+                    File.AppendAllText(path, " ");
+                    File.AppendAllText(path, userName);
+                    File.AppendAllText(path, " ");
+                    File.AppendAllText(path, pass);
+                    File.AppendAllText(path, "\n");
+                   
+                    Form1 f1 = new Form1();
+                    f1.Show();
+                    this.Hide();
+                }
             }
             else
             {
